080013fb73f07b46b854c9e9de5d0e081e521417
REVATHI104 <revathi.rece@ksrce.ac.in>
2016-08-09 04:54:25 -0700

    Create Intstring.java

---
+++ Intstring.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class Intstring {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the string : ");
+String s=in.next();
+System.out.println("Given string : "+s);
+int num=Integer.parseInt(s);
+System.out.println("Number is : "+num);
+
+
+	}
+
+}
