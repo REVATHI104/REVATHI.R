3266ca1270ecef33c35ff20365e7aae3f672a1a9
REVATHI104 <revathi.rece@ksrce.ac.in>
2016-08-09 23:19:38 -0700

    Create Workngday.java

---
+++ Workngday.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Workngday {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+String s=in.next();
+if((s.equals("Sunday"))||(s.equals("Saturday")))
+{
+	System.out.println("False");;
+}
+else
+{
+	System.out.println("True");
+}
+
+		
+		
+		
+	}
+
+}
