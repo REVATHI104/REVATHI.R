1b76d118ee47036ee850ec3a16d994a36b3c81c6
REVATHI104 <revathi.rece@ksrce.ac.in>
2016-08-10 01:25:24 -0700

    Create Length.java

---
+++ Length.java
@@ -0,0 +1,14 @@
+
+public class Length {
+
+	public static void main(String[] args) {
+String str=args[0];
+int length=str.length();
+System.out.println("Length of the given string is :"+length);
+		
+		
+		
+		
+	}
+
+}
