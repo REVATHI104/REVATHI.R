053016d7800f391cd9d9177a769e63a522e68a06
REVATHI104 <revathi.rece@ksrce.ac.in>
2016-08-30 02:36:03 -0700

    Create pnz.java

---
+++ pnz.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+public class pnz {
+
+	public static void main(String[] args) {
+Scanner in=new Scanner(System.in);
+System.out.println("Enter input : ");
+int num=in.nextInt();
+if(num>0)
+{ System.out.println("POSITIVE");}
+else if(num==0)
+{ System.out.println("ZERO");}
+else
+{System.out.println("NEGATIVE");
+
+}
+	}
+
+}
